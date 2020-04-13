bd608346a2a47e9ad9ef87a39ae46d89d614ee0c
Nyameaama Gambrah <nyameaama@Nyameaamas-MacBook-Pro.local>
2020-04-13 14:21:31 +0000

    Support for landing legs deployment

---
+++ src/NEO/Sensors/LandingLegs.cpp
@@ -0,0 +1,122 @@
+#include"LandingLegs.h"
+
+LLDeploy::LLDeploy(){
+    //Attaching Servos to pins
+    uint8_t num;
+    l1.attach(num);
+    l2.attach(num);
+    l3.attach(num);
+    l4.attach(num);
+}
+
+boolean LLDeploy::LLtrigger(){
+    //Function returns bool. If craft is above landing leg deploy margin,
+    //function returns false meaning that landing legs should be in retracted
+    //position but if function returns true craft is below margin, landing
+    //legs should be deployed.
+    boolean trigger = true;
+    uint8_t *ulSensor = (uint8_t*)malloc(2);
+    ulSensor[0] = TRIG_PIN;
+    ulSensor[1] = ECHO_PIN;
+    uint16_t groundLevel;
+    uint16_t ground_margin = groundLevel + 10;
+    uint16_t ultrasonics = ul.ultrasonic_sensor_calib( ulSensor[0],ulSensor[1]);
+    if(ultrasonics < ground_margin){
+        trigger = true;
+        free(ulSensor);
+    }else if(ultrasonics > ground_margin){
+        trigger = false;
+    }
+    free(ulSensor);
+    return trigger;
+}
+
+uint8_t LLDeploy::deploy(){
+    boolean trig = LLtrigger();
+    if(trig == true){
+        uint8_t iter = currentServosPos;
+        while(iter != DEPLOYED_POSITION){
+            sequentialMove(iter);
+            iter++;
+        }
+        currentServosPos = iter;
+    }else {
+        return 0;
+    }
+}
+
+uint8_t LLDeploy::retract(){
+    boolean trigger = LLtrigger();
+    if(trigger == false){
+        uint8_t count = currentServosPos;
+        while(count != RETRACTED_POSITION){
+            sequentialMove(count);
+            count--;
+        }
+        currentServosPos = count;
+    }else {
+        return 0;
+    }
+}
+
+uint8_t LLDeploy::sequentialMove(uint8_t iter){
+    uint8_t DELAY_TIME;
+    l1.write(iter);
+    delay(DELAY_TIME);
+    l2.write(iter);
+    delay(DELAY_TIME);
+    l3.write(iter);
+    delay(DELAY_TIME);
+    l4.write(iter);
+    delay(DELAY_TIME);
+    return 1;
+}
+
+uint8_t* LLDeploy::compilePos(){
+    uint8_t *positions = (uint8_t*)malloc(4);
+    for(int i = 0; i < 4;i++){
+        positions[i] = currentServosPos;
+    }
+    return positions;
+}
+
+boolean LLDeploy::verifyStatus(uint8_t x){
+    uint8_t *pos = compilePos();
+    boolean flag = true;
+    if(x == DEPLOYED_POSITION){
+        for(size_t i = 0;i < 4;i++){
+            if(pos[i] != DEPLOYED_POSITION){
+                flag = false;
+            }
+        }
+        free(pos);
+        return flag;
+    }
+    if(x == RETRACTED_POSITION){
+        for(size_t i = 0;i < 4;i++){
+            if(pos[i] != RETRACTED_POSITION){
+                flag = false;
+            }
+        }
+        free(pos);
+        return flag;
+    }
+}
+
+uint8_t LLDeploy::LL_controller(){
+    if(LLtrigger() == true){
+        deploy();
+        if(verifyStatus(DEPLOYED_POSITION) == true){
+            return 1;
+        }else{
+
+        }
+    }else if(LLtrigger() == false){
+        retract();
+        if(verifyStatus(RETRACTED_POSITION) == true){
+            return 1;
+        }else{
+
+        }
+    }    
+}
---
+++ src/NEO/Sensors/LandingLegs.h
@@ -0,0 +1,43 @@
+#include<Arduino.h>
+#include<Servo.h>
+#include"sensors.h"
+#include<stdint.h>
+#include<stddef.h>
+
+#define DEPLOYED_POSITION 90 // <-- Adjust position
+#define RETRACTED_POSITION 0 // Adjust
+#define ECHO_PIN 0
+#define TRIG_PIN 0 //<-- Add pin allocations
+
+class LLDeploy{
+    private:
+        Servo l1,l2,l3,l4;
+        uint8_t currentServosPos;
+        Sensors ul;
+
+    public:
+        LLDeploy();    
+
+    private:
+    //Function to move all servos into deployed position
+        uint8_t deploy();
+
+    //Function to move all servos into retracted position    
+        uint8_t retract();
+
+        uint8_t* compilePos();
+
+    //Function to verify deployment
+        boolean verifyStatus(uint8_t x);
+
+    //Function to flag when craft is in landing conditions
+        boolean LLtrigger();
+
+        uint8_t sequentialMove(uint8_t iter);
+
+    public:
+        //Landing Leg Controller
+        uint8_t LL_controller();
+
+
+};
