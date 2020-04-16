47e45e8c748ed75ba7411fe5e8730a819ca33575
Nyameaama Gambrah <nyameaama@Nyameaamas-MacBook-Pro.local>
2020-04-16 16:36:23 +0000

    Define Roll Control

--- src/NEO/Flight Control/Stability System/Roll Control/ActiveRollStabilization.h
+++ src/NEO/Flight Control/Stability System/Roll Control/ActiveRollStabilization.h
@@ -1,8 +1,15 @@
 #include<Servo.h>
 
+#define MOTOR_PIN_1 0
+#define MOTOR_PIN_2 0
+#define MOTOR_PIN_3 0
+
 class RollStability {
     private:
-        
+        Servo s1,s2,s3;
+
+    public:
+            
 
 
 };
