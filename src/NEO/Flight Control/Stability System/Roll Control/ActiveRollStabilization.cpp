5d3107c86a1e498aa641f9028f3f9c5a08d228f4
Nyameaama Gambrah <nyameaama@Nyameaamas-MacBook-Pro.local>
2020-04-18 00:07:32 +0000

    Active Roll Stabilization Improvements

--- src/NEO/Flight Control/Stability System/Roll Control/ActiveRollStabilization.cpp
+++ src/NEO/Flight Control/Stability System/Roll Control/ActiveRollStabilization.cpp
@@ -1,2 +1,37 @@
 #include"ActiveRollStabilization.h"
 
+String RollStability::determineRollDirection(){
+    //Roll direction can be calculated by using two roll values to
+    // 
+    uint8_t sampleAmount = 3;
+    double *rollV = compileValues(sampleAmount);
+    uint8_t count;
+    for(size_t i = 0; i < 2;i++){
+        if(rollV[i] < rollV[i + 1]){
+            count++;
+        }    
+    }
+    if(count == 2){
+      SPIN_DIRECTION = "anticlockwise"; 
+    }
+}
+
+uint8_t RollStability::updateCurrentRollAxis(){
+     uint8_t sampleAmount = 2;
+    double *Val = compileValues(sampleAmount);
+    if(Val[0] < 0){
+        POS_ROLL_AXIS = false;
+    }
+}
+double *RollStability::compileValues(uint8_t count){
+    Gyro rSen;
+    double *vals = (double*)malloc(count);
+    for(size_t i = 0;i < count;i++){
+        vals[i] = rSen.AccGyroVals(1);
+    }
+    return vals;
+}
+
+uint8_t computeCounterRoll(uint8_t x){
+    
+}
--- src/NEO/Flight Control/Stability System/Roll Control/ActiveRollStabilization.h
+++ src/NEO/Flight Control/Stability System/Roll Control/ActiveRollStabilization.h
@@ -1,15 +1,38 @@
 #include<Servo.h>
+#include"../../../Inertial Measurement/IMU.h"
 
 #define MOTOR_PIN_1 0
 #define MOTOR_PIN_2 0
 #define MOTOR_PIN_3 0
 
+#define NEG_THRESHOLD -179
+#define POS_THRESHOLD 179
+
+#define SPIN_STATE false
+
+//If rocket is spinning counter the direction of movement.
+
 class RollStability {
     private:
         Servo s1,s2,s3;
+        boolean POS_ROLL_AXIS = true;
+        String SPIN_DIRECTION= "clockwise";
+    private:
+        //Function to compile two roll values
+        double *compileValues(uint8_t count);
+
+        //Function to update wheter rocket is in negative or positive roll boundaries
+        uint8_t updateCurrentRollAxis();
+
+        //Function to determine roll direction
+        String determineRollDirection();
+
+        //Function to calculate counter movement for servo motors
+        uint8_t computeCounterRoll(uint8_t x);
 
     public:
-            
+        //Driver function which takes current roll values
+       uint8_t rollStabilize(uint8_t roll); 
 
 
 };
